0b5f260a46d69345e822b612ca3cda80bf70bb89
Tiago de Oliveira Januario <januarioccp@gmail.com>
2020-06-29 06:41:39 -0300

    Node implementation

---
+++ src/node.h
@@ -0,0 +1,22 @@
+//
+//  node.h
+//  PATAT
+//
+//  Created by Tiago Januario on 12/8/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+#ifndef PATAT_node_h
+#define PATAT_node_h
+
+//Classe de abstração de um vertice
+class Node{
+public:
+    //! Lista de cores dispon�veis para a colora��o deste v�rtice
+    int *corDisponivel;
+
+    //! Comprimento da lista de cores dispon�veis para a colora��o deste v�rtice
+    int size;
+
+};
+
+#endif
